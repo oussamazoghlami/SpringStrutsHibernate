bd3a5f30ae8b8b4f45a212bcd279b39f145a2678
Oussama Zoghlami <zoghlami.oussama@gmail.com>
2012-01-30 11:09:21 +0100

    Learning Guava

--- StrutsSpringExample/src/test/java/com/tunisiana/tutorials/util/TestGuava.java
+++ StrutsSpringExample/src/test/java/com/tunisiana/tutorials/util/TestGuava.java
@@ -12,10 +12,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.primitives.Ints;
@@ -27,7 +32,7 @@ import com.google.common.primitives.Ints;
  * 
  */
 public class TestGuava {
-
+	
 	@Test(expected = IllegalArgumentException.class)
 	public void testGuavaPreconditions() {
 		String name = null;
@@ -79,7 +84,7 @@ public class TestGuava {
 		// Define the loading cache
 		LoadingCache<String, String> cache = CacheBuilder.newBuilder().maximumSize(1000)
 				.expireAfterWrite(10, TimeUnit.MINUTES).build(greetingCacheLoader);
-		
+
 		assertEquals("Hello test", cache.get("test"));
 		assertEquals("Hello hahi", cache.get("hahi"));
 		cache.get("hahi");
@@ -87,6 +92,37 @@ public class TestGuava {
 		cache.get("hahi");
 		assertEquals(2, cache.stats().loadCount());
 		assertEquals(5, cache.stats().requestCount());
+	}
+
+	@Test
+	public void testFunctionalFunction() {
+		Function<String, Integer> lengthMapper = new Function<String, Integer>() {
+
+			@Override
+			public Integer apply(String input) {
+				return input == null ? 0 : input.length();
+			}
+		};
+		
+		assertEquals(0, (int) lengthMapper.apply(null));
+		assertEquals(0, (int) lengthMapper.apply(""));
+		assertEquals(4, (int) lengthMapper.apply("hahi"));
+	}
+	
+	@Test
+	public void testGuavaStringsUtil() {
+		// Test String Joiner
+		Joiner joiner = Joiner.on("; ").skipNulls();
+		String result = joiner.join("Hello","Mr","Ghost",null,"Man");
+		assertEquals("Hello; Mr; Ghost; Man", result);
+		
+		// Test Splitter Method
+		Splitter splitter = Splitter.on(",").omitEmptyStrings().trimResults();
+		assertEquals("[foo, bar, qux]", Iterables.toString(splitter.split("foo,bar,,   qux")));
+		
+		// CharMatcher
+		assertEquals("22275037", CharMatcher.DIGIT.retainFrom("Tel Number: 22275037"));
+		assertTrue(CharMatcher.JAVA_UPPER_CASE.matchesAllOf("AAABBBBCC"));
 	}
 
 }
